52ecb4e418039bd010ea142ee1088b18da0e64a7
juandiego07 <juan_diego07@hotmail.com>
2022-01-06 00:03:23 -0500

    Se agregan rutas para los controladores Session t Register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +12,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/register', 'App\Http\Controllers\RegisterController@create')->name('register');
+
+Route::get('/login', 'App\Http\Controllers\SessionController@create')->name('login');
