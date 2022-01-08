95ddc653546cf1de3a26d55df6a387d95e4f76d0
juandiego07 <juan_diego07@hotmail.com>
2022-01-08 12:31:39 -0500

    Se crea ruta para clientes

--- resources/views/layouts/client/index.blade.php
+++ resources/views/layouts/client/index.blade.php
@@ -14,62 +14,69 @@
                 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
                     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Add Client</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="row">
-                                    <div class="col-2 mb-2">
-                                        <label for="type_document" class="form-label">Type</label>
-                                        <select class="form-select" aria-label="Default select example" id="type_document" name="type_document">
-                                            <option selected></option>
-                                            <option value="NIT">NIT</option>
-                                            <option value="PEP">PEP</option>
-                                            <option value="CC">CC</option>
-                                            <option value="CE">CE</option>
-                                        </select>
+                        <form method="POST" action={{ route('client.store') }}>
+                            @csrf
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Add Client</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <div class="col-2 mb-2">
+                                            <label for="document_type" class="form-label">Type</label>
+                                            <select class="form-select" aria-label="Default select example"
+                                                id="document_type" name="document_type">
+                                                <option selected></option>
+                                                <option value="NIT">NIT</option>
+                                                <option value="PEP">PEP</option>
+                                                <option value="CC">CC</option>
+                                                <option value="CE">CE</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-5 mb-2">
+                                            <label for="document_number" class="form-label">Document Number</label>
+                                            <input type="number" class="form-control" id="document_number"
+                                                name="document_number" placeholder="1234567890">
+                                        </div>
+                                        <div class="col-5 mb-2">
+                                            <label for="phone_number" class="form-label">Phone Number</label>
+                                            <input type="number" class="form-control" id="phone_number"
+                                                name="phone_number" placeholder="3001234567">
+                                        </div>
                                     </div>
-                                    <div class="col-5 mb-2">
-                                        <label for="document_number" class="form-label">Document Number</label>
-                                        <input type="number" class="form-control" id="document_number" name="document_number"
-                                            placeholder="1234567890">
+                                    <div class="row">
+                                        <div class="col-6 mb-2">
+                                            <label for="name" class="form-label">Names</label>
+                                            <input type="text" class="form-control" id="name" name="name"
+                                                placeholder="Pepito Fulanito">
+                                        </div>
+                                        <div class="col-6 mb-2">
+                                            <label for="last_name" class="form-label">Last Name</label>
+                                            <input type="text" class="form-control" id="last_name" name="last_name"
+                                                placeholder="Perez Perez">
+                                        </div>
                                     </div>
-                                    <div class="col-5 mb-2">
-                                        <label for="phone_number" class="form-label">Phone Number</label>
-                                        <input type="number" class="form-control" id="phone_number" name="phone_number"
-                                            placeholder="3001234567">
+                                    <div class="row">
+                                        <div class="col-6 mb-2">
+                                            <label for="email" class="form-label">Email address</label>
+                                            <input type="email" class="form-control" id="email" name="email"
+                                                placeholder="name@example.com">
+                                        </div>
+                                        <div class="col-6 mb-2">
+                                            <label for="address" class="form-label">Address</label>
+                                            <input type="text" class="form-control" id="address" name="address"
+                                                placeholder="Calle 1 # 2 - 3, Medellin, Antioquia">
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-6 mb-2">
-                                        <label for="name" class="form-label">Names</label>
-                                        <input type="text" class="form-control" id="name" name="name" placeholder="Pepito Fulanito">
-                                    </div>
-                                    <div class="col-6 mb-2">
-                                        <label for="last_name" class="form-label">Last Name</label>
-                                        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Perez Perez">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-6 mb-2">
-                                        <label for="email" class="form-label">Email address</label>
-                                        <input type="email" class="form-control" id="email" name="email"
-                                            placeholder="name@example.com">
-                                    </div>
-                                    <div class="col-6 mb-2">
-                                        <label for="address" class="form-label">Address</label>
-                                        <input type="text" class="form-control" id="address" name="address" placeholder="Calle 1 # 2 - 3, Medellin, Antioquia">
-                                    </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Add</button>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Add</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -83,31 +90,25 @@
                 <table class="table table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Document</th>
+                            <th scope="col">Names</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Actinos</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>The Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        @foreach ($clients as $item)
+                            <tr>
+                                <td>{{ $item->document_type }}</td>
+                                <td>{{ $item->document_number }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->last_name }}</td>
+                                <td>{{ $item->phone_number }}</td>
+                                <td><i class="bi bi-pencil-square btn btn-primary"></i> | <i class="bi bi-x-square btn btn-danger"></i></td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
